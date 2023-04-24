90d099ae8b61f925289c9c3769cbd235888276d9
livingstone23 <livingstone23@gmail.com>
2023-04-24 18:09:13 +0200

    Complete Update Endpoint

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,29 +1,114 @@
 package user
 
 import (
-	"fmt"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"log"
 )
 
 /*Interface que recibe puntero de un usuario*/
 type Repository interface {
 	Create(user *User) error
+	GetAll() ([]User, error)
+	GetById(id string) (*User, error)
+	Delete(id string) error
+	Update(id string, firstName *string, lastName *string, email *string, phone *string) error
 }
 
 /*Estructura que hace referencia a la BD*/
 type repo struct {
-	db *gorm.DB
+	log *log.Logger
+	db  *gorm.DB
 }
 
 /*Funcion que se encarga de instanciar el repositorio de la bd, retorna un repositorio*/
-func NewRepo(db *gorm.DB) Repository {
+func NewRepo(log *log.Logger, db *gorm.DB) Repository {
 	return &repo{
-		db: db,
+		log: log,
+		db:  db,
 	}
 }
 
 /* Metodo */
 func (repo *repo) Create(user *User) error {
-	fmt.Println("repository")
+
+	user.ID = uuid.New().String()
+
+	/*
+		result := repo.db.Create(user)
+		if result.Error != nil {
+			repo.log.Println(result.Error)
+			return result.Error
+		}
+	*/
+
+	if err := repo.db.Create(user).Error; err != nil {
+		repo.log.Println(err)
+		return err
+	}
+
+	//fmt.Println("repository")
+	repo.log.Println("User created with id: ", user.ID)
+	return nil
+}
+
+func (repo *repo) GetAll() ([]User, error) {
+	var u []User
+
+	result := repo.db.Model(&u).Order("created_at desc").Find(&u)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return u, nil
+}
+
+func (repo *repo) GetById(id string) (*User, error) {
+	user := User{ID: id}
+
+	result := repo.db.First(&user)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &user, nil
+}
+
+func (repo *repo) Delete(id string) error {
+	user := User{ID: id}
+
+	result := repo.db.Delete(&user)
+
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (repo *repo) Update(id string, firstName *string, lastName *string, email *string, phone *string) error {
+	values := make(map[string]interface{})
+
+	if firstName != nil {
+		values["first_name"] = *firstName
+	}
+
+	if lastName != nil {
+		values["lastName"] = *lastName
+	}
+
+	if email != nil {
+		values["email"] = *email
+	}
+
+	if phone != nil {
+		values["phone"] = *phone
+	}
+
+	if err := repo.db.Model(&User{}).Where("id = ?", id).Updates(values).Error; err != nil {
+		return err
+	}
+
 	return nil
 }
--- internal/user/service.go
+++ internal/user/service.go
@@ -10,7 +10,6 @@ type Service interface {
 	Delete(id string) error
 	Update(id string, firstName *string, lastName *string, email *string, phone *string) error
 }
-
 type service struct {
 	log  *log.Logger
 	repo Repository
@@ -23,8 +22,50 @@ func NewService(log *log.Logger, repo Repository) Service {
 	}
 }
 
-func (s service) Create(firstName, lastName, email, phone string) error {
-	log.Println("Create user Service")
-	s.repo.Create(&User{})
-	return nil
+func (s service) Create(firstName, lastName, email, phone string) (*User, error) {
+	//log.Println("Create user Service")
+
+	user := User{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Phone:     phone,
+	}
+
+	s.log.Println("User created by Service")
+
+	//Se le pasa la direccion en memoria de la variable arriba creada
+	//s.repo.Create(&user)
+
+	if err := s.repo.Create(&user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (s service) GetAll() ([]User, error) {
+	users, err := s.repo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (s service) GetById(id string) (*User, error) {
+	user, err := s.repo.GetById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (s service) Delete(id string) error {
+	return s.repo.Delete(id)
+}
+
+func (s service) Update(id string, firstName *string, lastName *string, email *string, phone *string) error {
+	return s.repo.Update(id, firstName, lastName, email, phone)
 }
